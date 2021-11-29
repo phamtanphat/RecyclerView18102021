418bb56ca07b9fae8d2c466220be2a5e2766981f
phamtanphat <phatdroid94@gmail.com>
2021-11-29 19:13:09 +0700

    kiểm tra điều kiện đẻ load more

--- app/src/main/java/com/example/recyclerview18102021/PaginationScrollListener.java
+++ app/src/main/java/com/example/recyclerview18102021/PaginationScrollListener.java
@@ -1,19 +1,36 @@
 package com.example.recyclerview18102021;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 public abstract class PaginationScrollListener extends RecyclerView.OnScrollListener {
+
+    LinearLayoutManager linearLayoutManager;
+
+    public PaginationScrollListener(LinearLayoutManager linearLayoutManager){
+        this.linearLayoutManager = linearLayoutManager;
+    }
+
 
     @Override
     public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
         super.onScrolled(recyclerView, dx, dy);
 
+        int firstVisiblePosition = linearLayoutManager.findFirstVisibleItemPosition();
+        int totalItem = linearLayoutManager.getItemCount();
+        int visibleItem = linearLayoutManager.getChildCount();
+
         if (isLoading() || isLastPage()){
             return;
         }
+
+        if (firstVisiblePosition >= 0 && (visibleItem + firstVisiblePosition) >= totalItem){
+            loadMore();
+        }
     }
 
+    abstract public void loadMore();
     abstract public boolean isLoading();
     abstract public boolean isLastPage();
 }
